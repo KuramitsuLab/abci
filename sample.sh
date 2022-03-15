52fa2d752fe81d73a4dde057627b0a659b515c89
kkuramitsu <kkuramitsu@gmail.com>
2022-03-15 18:04:38 +0900

    fix

--- sample.sh
+++ sample.sh
@@ -3,6 +3,8 @@
 source /etc/profile.d/modules.sh
 module load gcc/9.3.0 python/3.8 cuda/11.2 cudnn/8.1
 
+export LD_LIBRARY_PATH=/lib:/usr/lib:/usr/local/lib:/apps/centos7/python/3.8.7/lib
+
 python3 train_mt5.py\
     --name='mt5_35'\
     --masking --masking_ratio=0.35\
